7e5bb849bcf94071144f1fbf6487677270e3d95b
yogahermawan <yogahermawan43@gmail.com>
2019-02-21 22:29:36 +0700

    first commit

---
+++ application/views/temp/index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> 
+<![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="en"> 
+<![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <title>Universe - College Education Responsive Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="College Education Responsive Template">
+    <meta name="author" content="Esmet">
+    <meta charset="UTF-8">
+
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800' rel='stylesheet' type='text/css'>
+        
+    <!-- CSS Bootstrap & Custom -->
+    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="../css/font-awesome.min.css" rel="stylesheet" media="screen">
+    <link href="../css/animate.css" rel="stylesheet" media="screen">
+    <link href="../">
+    
+    <link href="style.css" rel="stylesheet" media="screen">
+        
+    <!-- Favicons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../images/ico/favicon.ico">
+    
+    <!-- JavaScripts -->
+    <script src="../js/jquery-1.10.2.min.js"></script>
+    <script src="../js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="../js/modernizr.js"></script>
+    <!--[if lt IE 8]>
+	<div style=' clear: both; text-align:center; position: relative;'>
+            <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" alt="" /></a>
+        </div>
+    <![endif]-->
+</head>
+<body>
+
+    <!-- This one in here is responsive menu for tablet and mobiles -->
+    <div class="responsive-navigation visible-sm visible-xs">
+        <a href="#" class="menu-toggle-btn">
+            <i class="fa fa-bars"></i>
+        </a>
+        <div class="responsive_menu">
+            <ul class="main_menu">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#">Cari Kampus</a></li>
+                <li><a href="#">Informasi Pendaftaran</a></li>
+                <li><a href="#">Informasi Beasiswa</a></li>
+                <!-- /.main_menu -->
+                
+                <!-- /.social_icons -->
+        </div> <!-- /.responsive_menu -->
+    </div> <!-- /responsive_navigation -->
+
+
+    <header class="site-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 header-left">
+                    <p><i class="fa fa-phone"></i> +01 2334 853</p>
+                    <p><i class="fa fa-envelope"></i> <a href="mailto:email@universe.com">email@universe.com</a></p>
+                </div> <!-- /.header-left -->
+
+                <div class="col-md-4">
+                    <div class="logo">
+                        <a href="index.php" title="Universe" rel="home">
+                            <img src="images/logo.png" alt="Universe">
+                        </a>
+                    </div> <!-- /.logo -->
+                </div> <!-- /.col-md-4 -->
+
+                <div class="col-md-4 header-right">
+                    <ul class="small-links">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Apply Now</a></li>
+                    </ul>
+                    <div class="search-form">
+                        <form name="search_form" method="get" action="#" class="search_form">
+                            <input type="text" name="s" placeholder="Search the site..." title="Search the site..." class="field_search">
+                        </form>
+                    </div>
+                </div> <!-- /.header-right -->
+            </div>
+        </div> <!-- /.container -->
+
+        <div class="nav-bar-main" role="navigation">
+            <div class="container">
+                <nav class="main-navigation clearfix visible-md visible-lg" role="navigation">
+                        <ul class="main-menu sf-menu">   
+                            <li class="active"><a href="index.html">Home</a></li>
+                            <li><a href="#">Cari Kampus</a></li>
+                            <li><a href="#">Informasi Pendaftaran</a></li>
+                            <li><a href="#">Informasi Beasiswa</a></li>
+                            
+                            <!-- /.main-menu -->
+                            <div class="responsive-navigation visible-sm visible-xs">
+                                    <a href="#" class="menu-toggle-btn">
+                                        <i class="fa fa-bars"></i>
+                                    </a>
+                                    <div class="responsive_menu">
+                                        <ul class="main_menu">
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="#">Cari Kampus</a></li>
+                                            <li><a href="#">Informasi Pendaftaran</a></li>
+                                            <li><a href="#">Informasi Beasiswa</a></li>
+                                            
+                                            <!-- /.main_menu -->
+
+                                            <!-- /.social_icons -->
+                                    </div> <!-- /.responsive_menu -->
+                            </div>
+                            <!-- /.social-icons -->
+                </nav> <!-- /.main-navigation -->
+            </div> <!-- /.container -->
+        </div> <!-- /.nav-bar-main -->
+
+    </header> <!-- /.site-header -->
+    
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="main-slideshow">
+                    <div class="flexslider">
+                        <ul class="slides">
+                            <li>
+                                <img src="http://placehold.it/770x400" alt="Slide 1"/>
+                                <div class="slider-caption">
+                                    <h2><a href="blog-single.html">When a Doctor’s Visit Is a Guilt Trip</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="http://placehold.it/770x400" alt="Slide 1"/>
+                                <div class="slider-caption">
+                                    <h2><a href="blog-single.html">Unlocking the scrolls of Herculaneum</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="http://placehold.it/770x400" alt="Slide 1"/>
+                                <div class="slider-caption">
+                                    <h2><a href="blog-single.html">Corin Sworn wins Max Mara Art Prize</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                </div>
+                            </li>
+                        </ul> <!-- /.slides -->
+                    </div> <!-- /.flexslider -->
+                </div> <!-- /.main-slideshow -->
+            </div> <!-- /.col-md-12 -->
+            
+            <div class="col-md-4">
+                <div class="widget-item">
+                    <div class="request-information">
+                        <h4 class="widget-title">Cari Kampus</h4>
+                        <form class="request-info clearfix"> 
+                            <div class="full-row">
+                                <label for="cat">Jurusan Yang Diminati :</label>                
+                                <div class="input-select">
+                                    <select name="cat" id="cat" class="postform">
+                                        <option value="-1">-- Pilih Jurusan --</option>
+                                          <option>Jurusan 1</option>
+                                          <option>Jurusan 2</option>
+                                          <option>Jurusan 3</option>
+                                    </select>
+                                </div> <!-- /.input-select -->  
+                            </div> <!-- /.full-row -->
+
+                            <div class="full-row">
+                                <label for="cat2">Jenjang Studi :</label>
+                                <div class="input-select">
+                                    <select name="cat2" id="cat2" class="postform">
+                                        <option value="-1">-- Pilih Jenjang --</option>
+                                        <option class="level-0" value="49">Diploma 3</option>
+                                        <option class="level-0" value="56">Strata 1</option>
+                                        <option class="level-0" value="59">Strata 2</option>
+                                        </select>
+                                </div> <!-- /.input-select -->
+                            </div> <!-- /.full-row -->
+
+                            <div class="full-row">
+                                    <label for="cat2">Kategori Kampus :</label>
+                                    <div class="input-select">
+                                        <select name="cat2" id="cat2" class="postform">
+                                            <option value="-1">-- Pilih Kategori --</option>
+                                            <option class="level-0" value="49">Kategori 1</option>
+                                            <option class="level-0" value="56">Kategori 2</option>
+                                            <option class="level-0" value="59">Kategori 3</option>
+                                            </select>
+                                    </div>
+                            </div>
+
+                            <div class="full-row">
+                                <label for="cat2">Propinsi :</label>
+                                <div class="input-select">
+                                    <select name="cat2" id="cat2" class="postform">
+                                        <option value="-1">-- Pilih Propinsi --</option>
+                                        <option class="level-0" value="49">Propinsi 1</option>
+                                        <option class="level-0" value="56">Propinsi 2</option>
+                                        <option class="level-0" value="59">Propinsi 3</option>
+                                        </select>
+                                </div>
+                            </div>
+                            
+                            <div class="full-row">
+                                <div class="submit_field">
+                                    <input class="mainBtn pull-right" type="submit" name="" value="Cari">
+                                </div> <!-- /.submit-field -->
+                            </div> <!-- /.full-row -->
+
+
+                        </form> <!-- /.request-info -->
+                    </div> <!-- /.request-information -->
+                </div> <!-- /.widget-item -->
+            </div> <!-- /.col-md-4 -->
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <!-- ===+++++===== konten jika ingin ditambah posisi ditengah===++++==== -->
+        </div>
+    </div>
+
+    <!-- begin The Footer -->
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="footer-widget">
+                        <h4 class="footer-widget-title">Hubungi Kami</h4>
+                        <h4 class="footer-widget-title">Deskripsi</h4>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="footer-widget">
+                        <h4 class="footer-widget-title">Profinsi Terpopuler</h4>
+                        <ul class="list-links">
+                            <li><a href="#">Yogyakarta</a></li>
+                            <li><a href="#">Jawa Barat</a></li>
+                            <li><a href="#">Jawa Tengah</a></li>
+                            <li><a href="#">Jawa Timur</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="footer-widget">
+                        <h4 class="footer-widget-title">Tipe Kampus</h4>
+                        <ul class="list-links">
+                            <li><a href="#">Swasta</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="footer-widget">
+                        <h4 class="footer-widget-title">Kategori Kampus</h4>
+                        <ul class="list-links">
+                            <li><a href="#">Akakdemik</a></li>
+                            <li><a href="#">Institut</a></li>
+                            <li><a href="#">Sekolah Tinggi</a></li>
+                            <li><a href="#">Universitas</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="footer-widget">
+                        <ul class="footer-media-icons">
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <li><a href="#" class="fa fa-twitter"></a></li>
+                            <li><a href="#" class="fa fa-google-plus"></a></li>
+                            <li><a href="#" class="fa fa-youtube"></a></li>
+                            <li><a href="#" class="fa fa-linkedin"></a></li>
+                            <li><a href="#" class="fa fa-instagram"></a></li>
+                            <li><a href="#" class="fa fa-apple"></a></li>
+                            <li><a href="#" class="fa fa-rss"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div> <!-- /.row -->
+
+            <div class="bottom-footer">
+                <div class="row">
+                    <div class="col-md-5">
+                        <p class="small-text">&copy; Copyright 2014. Universe designed by <a href="#">Esmeth</a></p>
+                    </div> <!-- /.col-md-5 -->
+                    <!-- /.col-md-7 -->
+                </div> <!-- /.row -->
+            </div> <!-- /.bottom-footer -->
+
+        </div> <!-- /.container -->
+    </footer> <!-- /.site-footer -->
+
+
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../js/plugins.js"></script>
+    <script src="../js/custom.js"></script>
+
+</body>
+</html>
